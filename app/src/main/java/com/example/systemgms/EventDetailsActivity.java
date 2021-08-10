226656cddce1e73756fe90e2c97967333b065681
kamaubenjamin <benjaminkamauu@gmail.com>
2021-08-10 13:13:26 +0300

    commit 2

--- app/src/main/java/com/example/systemgms/EventDetailsActivity.java
+++ app/src/main/java/com/example/systemgms/EventDetailsActivity.java
@@ -1,6 +1,15 @@
 package com.example.systemgms;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EventDetailsActivity  extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eventdetails);
+    }
 }
