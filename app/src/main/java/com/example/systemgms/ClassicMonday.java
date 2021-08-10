fde3dbf2b5f47cb7b740ad153ea9ecd64958bc18
kamaubenjamin <benjaminkamauu@gmail.com>
2021-08-10 16:00:18 +0300

    first commit

--- app/src/main/java/com/example/systemgms/ClassicMonday.java
+++ app/src/main/java/com/example/systemgms/ClassicMonday.java
@@ -1,4 +1,15 @@
 package com.example.systemgms;
 
-public class ClassicMonday {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ClassicMonday extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classicmonday);
+    }
 }
